ef2059c41be570068b4b761b8c1c936e763586e5
Erwann <erwann.martin@gmail.com>
2016-05-02 11:47:45 +0200

    bsp

--- src/bsp.ml
+++ src/bsp.ml
@@ -1,11 +1,28 @@
 open Segment
+open Point
 
 type t = E | N of Segment.t * t * t 
 
-let parse f bsp p = failwith "TODO"
+let gauche segment point=
+	let res = (segment.pdest.x - segment.porig.x) * (point.y - segment.porig.y) - (segment.pdest.y - segment.porig.y)* (point.x - segment.porig.x) in
+	if res > 0 then true
+	else false
 
-let rev_parse f bsp p = failwith "TODO"
+let rec parse f bsp p = 
+	match bsp with
+	| E -> ()
+	| N (r, g, d) -> if gauche r p then (parse f g p; f r; parse f d p)
+					else (parse f d p; f r; parse f g p)
 
-let iter f bsp = failwith "TODO"
+let rec rev_parse f bsp p = match bsp with
+	| E -> ()
+	| N (r, g, d) -> if not (gauche r p) then (rev_parse f g p; f r; rev_parse f d p)
+					else (rev_parse f d p; f r; rev_parse f g p)
+
+
+let iter f =  function 
+	| E -> ()
+	| N (r, g, d) -> parse f g ; f r; parse f d p
+					
 
 let build_bsp sl = failwith "TODO"
