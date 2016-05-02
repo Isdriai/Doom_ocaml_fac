9d474b24ff0b570610cb242e21d61c02bfadf021
Erwann <erwann.martin@gmail.com>
2016-05-02 12:14:05 +0200

    plus qu'a opti

--- src/bsp.ml
+++ src/bsp.ml
@@ -3,26 +3,30 @@ open Point
 
 type t = E | N of Segment.t * t * t 
 
-let gauche segment point=
-	let res = (segment.pdest.x - segment.porig.x) * (point.y - segment.porig.y) - (segment.pdest.y - segment.porig.y)* (point.x - segment.porig.x) in
-	if res > 0 then true
-	else false
+
 
 let rec parse f bsp p = 
 	match bsp with
 	| E -> ()
-	| N (r, g, d) -> if gauche r p then (parse f g p; f r; parse f d p)
+	| N (r, g, d) -> if get_position p r = L then (parse f g p; f r; parse f d p)
 					else (parse f d p; f r; parse f g p)
 
 let rec rev_parse f bsp p = match bsp with
 	| E -> ()
-	| N (r, g, d) -> if not (gauche r p) then (rev_parse f g p; f r; rev_parse f d p)
+	| N (r, g, d) -> if not (get_position p r = L) then (rev_parse f g p; f r; rev_parse f d p)
 					else (rev_parse f d p; f r; rev_parse f g p)
 
 
-let iter f =  function 
+let rec iter f =  function 
 	| E -> ()
-	| N (r, g, d) -> parse f g ; f r; parse f d p
+	| N (r, g, d) -> iter f g ; f r; iter f d
 					
 
-let build_bsp sl = failwith "TODO"
+
+let build_bsp sl = 
+	let rec b_bsp = function
+		| []-> E
+		| x::s -> let (ll, lr) = split x s in N (x, b_bsp ll, b_bsp lr)
+	in
+	b_bsp sl
+
