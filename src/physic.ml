3fa040e55f2be783044a44c44d8f36678215f374
Erwann <erwann.martin@gmail.com>
2016-05-03 12:07:15 +0200

    plus qu'a opti

--- src/physic.ml
+++ src/physic.ml
@@ -1,15 +1,20 @@
 open Bsp
+open Options
+open Point
+open Segment
 
-let surSegment p s =
-	get_position p s && new_segment
+let angle (xa, ya) (xb, yb) (xc, yc) =
+	(xb -. xa) *. (xc -. xa) -. (yb -. ya) *. (yc -. ya)
+
+let procheSegment p =
+	fun s -> let (xc, yc) = float_of_int p.x, float_of_int p.y in
+			let (xa, ya) = float_of_int s.porig.x, float_of_int s.porig.y in
+			let (xb, yb) = float_of_int s.pdest.x, float_of_int s.pdest.y in
+			if sin (acos (angle (xa, ya) (xb, yb) (xc, yc))) < Options.step_dist then raise Exit
+
 
 let detect_collision p bsp = 
-
-	let d_t pre = function
-	 match bsp with
-	 | E -> SurSegment p pre
-	 | N (r, g, d) -> if get_position p r = L then detect_collision p pre g
-					else detect_collision p pre d
-	in
-
-	d_t (new_segment 0 0 0 0) bsp
+	try
+		Bsp.parse (procheSegment p) bsp p; false
+	with
+	| _ -> true
