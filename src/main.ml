71730db3cbee17cf2e3a1902c5b5450bec98530a
Rodolphe_Picot <picot.rodolphe@gmail.com>
2016-05-04 16:14:20 +0200

    attente ds le main

--- src/main.ml
+++ src/main.ml
@@ -2,7 +2,8 @@ open Test
 open Graphics
 
 let () = 
-	test ()
-	let s = Graphics.wait_next_event [Graphics.Button_down;Graphics.Key_pressed] in ()
+	test ();
+	let s = Graphics.wait_next_event [Graphics.Button_down;Graphics.Key_pressed]
+	 in ()
 
 ;;
