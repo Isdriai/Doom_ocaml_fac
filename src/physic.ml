ba93c9a479a81c03fa319110d7d52695283e5553
Erwann <erwann.martin@gmail.com>
2016-05-09 16:06:16 +0200

    correction léger bug

--- src/physic.ml
+++ src/physic.ml
@@ -5,8 +5,8 @@ let detect_collision p bsp =
 	let rec dc f bsp = 
 		match bsp with
 		| E -> ()
-		| N (r, g, d) -> if get_position p r = L then (parse f g p; f r)
-						else (parse f d p; f r)
+		| N (r, g, d) -> if get_position p r = L then (dc f g; f r)
+						else (dc f d; f r)
 	in
 	try 
 		dc (Segment.dansLaBoite p) bsp; false
